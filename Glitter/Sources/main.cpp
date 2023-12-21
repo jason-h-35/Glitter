6f5cd4d3e1f0a11f19d2989302a4a89b2fbaef39
Jason Hatton <jason@hatton.io>
2023-12-21 16:43:36 -0600

    troubleshooting shader compilation err

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -16,13 +16,15 @@ const char *vertexShaderSource =
 
 const char *fragmentShaderSource =
     "#version 330 core\n"
-    "out vec4 FragColor;\n\n"
+    "out vec4 FragColor;\n"
     "void main() {\n"
     "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f)\n"
     "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+bool shaderOK(unsigned int shader);
+bool programOK(unsigned int program);
 
 int main() {
   glfwInit();
@@ -49,43 +51,87 @@ int main() {
   // make 1 vertex buffer object.
   // VBO should be an array of ids to vertex buffer objects
   // But it can be just an int here b/c C++, I guess...
-  unsigned int VBO;
+  unsigned int VAO, VBO;
+  // generate VAO and VBO
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  // bind VAO to GL context
+  glBindVertexArray(VAO);
+  // bind VBO to GL context as array buffer
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // ingest vertices into the array buffer bound to GL context (VBO)
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  // create a shader and compile it
+  // create Vertex shader and compile it
   unsigned int vertexShader;
   vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
   glCompileShader(vertexShader);
   // check if it worked
-  int success;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(vertexShader, strlen(infoLog), NULL, infoLog);
-    std::cout << "error in shader vertex. compilation failed\n"
-              << infoLog << std::endl;
+  if (!shaderOK(vertexShader)) {
+    std::cout << "vertex shader failed to compile" << std::endl;
   }
-  int count = 0;
+  // create Fragment shader and compile it
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  // check if it worked
+  if (!shaderOK(fragmentShader)) {
+    std::cout << "fragment shader failed to compile" << std::endl;
+  }
+  // create shader program, attach V and F shader to it.
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  // Link program to GL context
+  glLinkProgram(shaderProgram);
+  if (!programOK(shaderProgram)) {
+    std::cout << "shader program failed to compile" << std::endl;
+  }
+  // Make it primary program in GL context
+  glUseProgram(shaderProgram);
+  // Clean up.
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  // Give GL the start index, size, type, normalized?, stride, ptr
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // Make a VAO (vertex array object) which is like an array of
+  // (isVertexAttribArrayEnabled, glVertexAttribPointer, VBO)
   while (!glfwWindowShouldClose(window)) {
     // input
     processInput(window);
     // rendering goes here
-    if (count % 3 == 0)
-      glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // sets the clear color in GL state
-    if (count % 3 == 1)
-      glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // sets the clear color in GL state
-    if (count % 3 == 2)
-      glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // sets the clear color in GL state
-    glClear(GL_COLOR_BUFFER_BIT); // uses values from glClearColor to glClear
-    count++;
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
   glfwTerminate();
   return 0;
+}
+
+bool shaderOK(unsigned int shader) {
+  int success;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    // glGetShaderInfoLog(shader, strlen(infoLog), NULL, infoLog);
+    return false;
+  }
+  return true;
+}
+
+bool programOK(unsigned int program) {
+  int success;
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    // glGetProgramInfoLog(program, strlen(infoLog), NULL, infoLog);
+    return false;
+  }
+  return true;
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
