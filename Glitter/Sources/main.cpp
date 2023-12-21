308d53c557c228a3ec3540236af974dd15ce18b2
Jason Hatton <jason@hatton.io>
2023-12-20 23:33:09 -0600

    Hello Window finished

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,49 +1,51 @@
-// Local Headers
-#include "glitter.hpp"
-
-// System Headers
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
 // Standard Headers
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
-  // Load GLFW and Create a Window
+int main() {
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  // window hints are like a key-value setter
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  // core profile means reduce OpenGL down to the modern subset
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-  auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
-
-  // Check for Valid Context
-  if (mWindow == nullptr) {
-    fprintf(stderr, "Failed to Create OpenGL Context");
-    return EXIT_FAILURE;
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
   }
-
-  // Create Context and Load OpenGL Functions
-  glfwMakeContextCurrent(mWindow);
-  gladLoadGL();
-  fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
-
-  // Rendering Loop
-  while (glfwWindowShouldClose(mWindow) == false) {
-    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(mWindow, true);
-
-    // Background Fill Color
-    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Flip Buffers and Draw
-    glfwSwapBuffers(mWindow);
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    processInput(window);
+    // rendering goes here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // sets the clear color in GL state
+    glClear(GL_COLOR_BUFFER_BIT); // uses values from glClearColor to glClear
+    // check and call events and swap the buffers
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
   glfwTerminate();
-  return EXIT_SUCCESS;
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
