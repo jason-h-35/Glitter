05fe94662d1fb25942bfd63b095f6f3b19dd24cd
Jason Hatton <jason@hatton.io>
2023-12-21 15:51:30 -0600

    Hello Triangle up to Frag shader

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,8 +1,25 @@
 #include <GLFW/glfw3.h>
+#include <cstring>
 #include <glad/glad.h>
 
 // Standard Headers
 #include <iostream>
+
+// shader string
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n\n"
+    "void main() {\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f)\n"
+    "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -27,12 +44,42 @@ int main() {
   }
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // make a triangle w/ verts
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  // make 1 vertex buffer object.
+  // VBO should be an array of ids to vertex buffer objects
+  // But it can be just an int here b/c C++, I guess...
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // create a shader and compile it
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  // check if it worked
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, strlen(infoLog), NULL, infoLog);
+    std::cout << "error in shader vertex. compilation failed\n"
+              << infoLog << std::endl;
+  }
+  int count = 0;
   while (!glfwWindowShouldClose(window)) {
     // input
     processInput(window);
     // rendering goes here
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // sets the clear color in GL state
+    if (count % 3 == 0)
+      glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // sets the clear color in GL state
+    if (count % 3 == 1)
+      glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // sets the clear color in GL state
+    if (count % 3 == 2)
+      glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // sets the clear color in GL state
     glClear(GL_COLOR_BUFFER_BIT); // uses values from glClearColor to glClear
+    count++;
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
