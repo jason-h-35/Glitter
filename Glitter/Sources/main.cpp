605292fba4ba3ebc8af13c315ad98d4507179a3f
Jason Hatton <jason@hatton.io>
2023-12-21 16:49:40 -0600

    it was a semicolon in frag shader.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -17,8 +17,9 @@ const char *vertexShaderSource =
 const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "void main() {\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f)\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
