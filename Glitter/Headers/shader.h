a50e63a13f437c20a73582d82e1702afcaf9cd90
Jason Hatton <jason@hatton.io>
2023-12-27 15:43:21 -0600

    shader constructor

--- Glitter/Headers/shader.h
+++ Glitter/Headers/shader.h
@@ -10,7 +10,34 @@ class Shader {
 public:
   unsigned int ID;
   // constructor reads and builds shader
-  Shader(const char *vertexPath, const char *fragmentPath);
+  Shader(const char *vertexPath, const char *fragmentPath) {
+    // 1. retrieve shader source code from file path
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+      vShaderFile.open(vertexPath);
+      fShaderFile.open(fragmentPath);
+      std::stringstream vShaderStream, fShaderStream;
+      // read ifstreams into stringsteams
+      vShaderStream << vShaderFile.rdbuf();
+      fShaderStream << fShaderFile.rdbuf();
+      // close ifstreams
+      vShaderFile.close();
+      fShaderFile.close();
+      // convert stringstream to string
+      vertexCode = vShaderStream.str();
+      fragmentCode = fShaderStream.str();
+    } catch (std::ifstream::failure e) {
+      std::cerr << "ERROR::SHADER::FILE_READ_NOT_OK" << std::endl;
+    }
+    const char *vShaderCode = vertexCode.c_str();
+    const char *fShaderCode = fragmentCode.c_str();
+  }
   // use/activate the shader
   void use();
   // utility uniform functions
