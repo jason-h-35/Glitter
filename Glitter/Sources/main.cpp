863b5d9464d110602ae0930b3707201bcd79b924
Jason Hatton <jason@hatton.io>
2023-12-28 23:04:44 -0600

    refactor to use Shader class, set vert vals, and some minor things

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,29 +1,15 @@
 #include <GLFW/glfw3.h>
 #include <cstring>
 #include <glad/glad.h>
+#include <shader.h>
 
-// Standard Headers
 #include <iostream>
-const char *orangeFragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
-const char *yellowFragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-    "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
-unsigned int genVertexShader();
-unsigned int genFragmentShader(const char *source);
-bool programOK(unsigned int program);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main() {
   glfwInit();
@@ -37,42 +23,27 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  float verts1[] = {0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f};
-  float verts2[] = {0.2f, -0.5f, 0.0f, -0.8f, -0.5f, 0.0f, -0.8f, 0.5f, 0.0f};
+  Shader ourShader("shader.vert", "shader.frag");
+  glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+  // clang-format off
+  float vertices[] = {
+    // positions          // colors
+     0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom left
+     0.0f, 0.5f,  0.0f,   0.0f, 0.0f, 1.0f  // top
+  };
+  // clang-format on
   unsigned int vertBuf[2];
   glGenBuffers(2, vertBuf);
   glBindBuffer(GL_ARRAY_BUFFER, vertBuf[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(verts1), verts1, GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, vertBuf[1]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(verts2), verts2, GL_STATIC_DRAW);
-  unsigned int vertexShader = genVertexShader();
-
-  unsigned int orangeFragShader = genFragmentShader(orangeFragmentShaderSource);
-  unsigned int orangeProgram = glCreateProgram();
-  glAttachShader(orangeProgram, vertexShader);
-  glAttachShader(orangeProgram, orangeFragShader);
-  glLinkProgram(orangeProgram);
-  if (!programOK(orangeProgram)) {
-    std::cout << "shader program failed to compile" << std::endl;
-  }
-  glDeleteShader(orangeFragShader);
-
-  unsigned int yellowFragShader = genFragmentShader(yellowFragmentShaderSource);
-  unsigned int yellowProgram = glCreateProgram();
-  glAttachShader(yellowProgram, vertexShader);
-  glAttachShader(yellowProgram, yellowFragShader);
-  glLinkProgram(yellowProgram);
-  if (!programOK(yellowProgram)) {
-    std::cout << "shader program failed to compile" << std::endl;
-  }
-  glDeleteShader(yellowFragShader);
-
   unsigned int vertAttr[2];
   glGenVertexArrays(2, vertAttr);
   glBindVertexArray(vertAttr[0]);
@@ -87,7 +58,7 @@ int main() {
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
-    glUseProgram(orangeProgram);
+    glUseProgram(program);
     glBindVertexArray(vertAttr[0]);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glUseProgram(yellowProgram);
@@ -98,51 +69,6 @@ int main() {
   }
   glfwTerminate();
   return 0;
-}
-
-unsigned int genVertexShader() {
-  const char *vertexShaderSource =
-      "#version 330 core\n"
-      "layout (location = 0) in vec3 aPos;\n"
-      "void main()\n"
-      "{\n"
-      "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-      "}\0";
-  unsigned int ref;
-  ref = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(ref, 1, &vertexShaderSource, NULL);
-  glCompileShader(ref);
-  int ok;
-  glGetShaderiv(ref, GL_COMPILE_STATUS, &ok);
-  if (!ok) {
-    std::cout << "vertex shader failed to compile" << std::endl;
-    // glGetShaderInfoLog(shader, strlen(infoLog), NULL, infoLog);
-  }
-  return ref;
-}
-
-unsigned int genFragmentShader(const char *source) {
-  unsigned int ref;
-  ref = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(ref, 1, &source, NULL);
-  glCompileShader(ref);
-  int ok;
-  glGetShaderiv(ref, GL_COMPILE_STATUS, &ok);
-  if (!ok) {
-    std::cout << "fragment shader failed to compile" << std::endl;
-    // glGetShaderInfoLog(shader, strlen(infoLog), NULL, infoLog);
-  }
-  return ref;
-}
-
-bool programOK(unsigned int program) {
-  int success;
-  glGetProgramiv(program, GL_LINK_STATUS, &success);
-  if (!success) {
-    // glGetProgramInfoLog(program, strlen(infoLog), NULL, infoLog);
-    return false;
-  }
-  return true;
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
